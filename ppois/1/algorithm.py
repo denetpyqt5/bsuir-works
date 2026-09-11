d914a5e0a76c6ff0ef3b1aadf1a7b1b1112ca602
denetpyqt5 <aiengineermain@gmail.com>
2026-09-11 10:49:42 +0300

    added UserInterface in pyatnashki.py, moved class ValidInput in file input.py

--- ppois/1/algorithm.py
+++ ppois/1/algorithm.py
@@ -1,15 +1,6 @@
+from input import ValidInput
+
 # Нормальные алгоритмы Маркова
-
-
-class ValidInput:
-    def valid_int_input(self, left: int, right: int, text: str) -> int:
-        while True:
-            try:
-                data = int(input(text))
-                if left <= data <= right:
-                    return data
-            except Exception:
-                print("Wrong data. Try one more time")
 
 
 class Alphabet:
--- ppois/1/pyatnashki.py
+++ ppois/1/pyatnashki.py
@@ -1,4 +1,5 @@
 from random import shuffle
+from input import ValidInput
 
 
 # Пятнашки
@@ -7,7 +8,9 @@ class Fives:
     def __init__(self):
         self.correct_matrix = [x for x in range(9)]
         self.current_matrix = [x for x in range(9)]
+
         self.ready_to_move = []
+
         shuffle(self.current_matrix)
 
     def __str__(self):
@@ -20,16 +23,29 @@ class Fives:
         index = self.current_matrix.index(0)
         line = 1 if index < 3 else 2 if index < 6 else 3
 
-        self.ready_to_move.extend([self.current_matrix[index - 3], self.current_matrix[index + 3]] if line == 2 else
-                                  [self.current_matrix[index + 3]] if line == 1 else
-                                  [self.current_matrix[index - 3]])
+        # Вертикальные соседи
+        if line == 2:
+            self.ready_to_move.extend([
+                self.current_matrix[index - 3],
+                self.current_matrix[index + 3]
+            ])
+        elif line == 1:
+            self.ready_to_move.append(self.current_matrix[index + 3])
+        else:
+            self.ready_to_move.append(self.current_matrix[index - 3])
 
-        self.ready_to_move.extend([self.current_matrix[index - 1]] if (index + 1) % 3 == 0 else
-                                  [self.current_matrix[index - 1], self.current_matrix[index + 1]] if (
-                                                                                                                  index + 2) % 3 == 0 else
-                                  [self.current_matrix[index + 1]])
+        # Горизонтальные соседи
+        if (index + 1) % 3 == 0:
+            self.ready_to_move.append(self.current_matrix[index - 1])
+        elif (index + 2) % 3 == 0:
+            self.ready_to_move.extend([
+                self.current_matrix[index - 1],
+                self.current_matrix[index + 1]
+            ])
+        else:
+            self.ready_to_move.append(self.current_matrix[index + 1])
 
-    def _choose(self):
+    def choose(self):
         print(self)
         data = int(input("Number to switch: "))
         self._move()
@@ -38,13 +54,27 @@ class Fives:
             self.current_matrix[self.current_matrix.index(0)] = data
             self.current_matrix[temp] = 0
 
-    def game_start(self):
-        while not self.__bool__():
-            self._choose()
+
+class UserInterface:
+    def __init__(self, five: Fives):
+        self.__five = five
+
+    def __game_start(self):
+        while not self.__five.__bool__():
+            self.__five.choose()
         else:
             print("You win")
+
+    def start_dialog(self):
+        print("1.Start game\n2.Exit")
+        info = ValidInput.valid_int_input(self, 1, 2, "Choose number: ")
+        if info == 1:
+            self.__game_start()
+        else:
+            return
 
 
 if __name__ == "__main__":
     game = Fives()
-    game.game_start()
+    interface = UserInterface(game)
+    interface.start_dialog()
